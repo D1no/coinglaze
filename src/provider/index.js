d94c7f26f989f0b06fdfe2d579b08d7655a383ea
Dino Scheidt <scheidtrd+github.com@gmail.com>
2019-01-19 20:01:22 +0100

    ✨👽 Making sure, that we have a retry strategy for (3rd party) api providers

--- src/provider/index.js
+++ src/provider/index.js
@@ -5,11 +5,29 @@
 import React from "react";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from "apollo-link";
 import { RestLink } from "apollo-link-rest";
+import { RetryLink } from "apollo-link-retry";
 import { ApolloProvider } from "react-apollo";
 import coinbaseConfig from "./coinbase/coinbaseConfig";
 
 const { baseUrl } = coinbaseConfig;
+
+/**
+ * Since we are dealing with a 3rd party API, we need to be careful with rate limiting
+ * I.e. coinbase will throw a 429 if over pulled
+ */
+const retryRest = new RetryLink({
+  delay: {
+    initial: 1000,
+    max: Infinity,
+    jitter: true
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error, _operation) => !!error
+  }
+});
 
 /**
  * Note: We could define multiple endpoints.
@@ -20,13 +38,24 @@ const restProviders = new RestLink({
   }
 })
 
-const restClient = new ApolloClient({
-  link: restProviders,
+/**
+ * Composing the link of the provider by chaining them
+ */
+const link = new ApolloLink.from([
+  retryRest,
+  restProviders
+])
+
+
+const Client = new ApolloClient({
+  link: link,
   cache: new InMemoryCache()
 })
 
+export { Client };
+
 const Provider = (props) => (
-  <ApolloProvider client={restClient}>
+  <ApolloProvider client={Client}>
     {props.children}
   </ApolloProvider>
 );

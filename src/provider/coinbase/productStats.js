87cefea733e25da357e68de28f9b6876ecdba39e
Dino Scheidt <scheidtrd+github.com@gmail.com>
2019-01-27 21:21:16 +0100

    🚨 Fix unused var lint

--- src/provider/coinbase/productStats.js
+++ src/provider/coinbase/productStats.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
-import { Client } from "../index";
 
 /**
  * Standard (slow) query, mitigated by provider against rate limiting
